e5a151e1186a53744467e0476813c7af498b25d0
Tatiana Getmanova <tatiana.getmanova@gamil.com>
2017-05-15 01:31:38 +0300

    Setup maven java project

---
+++ src/main/java/com/github/spb/tget/demo/data/UserInfo.java
@@ -0,0 +1,7 @@
+package com.github.spb.tget.demo.data;
+
+/**
+ * Created by tatiana on 15/05/2017.
+ */
+public class UserInfo {
+}
---
+++ src/main/java/com/github/spb/tget/demo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+/**
+ * Created by tatiana on 15/05/2017.
+ */
+package com.github.spb.tget.demo.repository;
+
+public interface UserRepository {
+}
