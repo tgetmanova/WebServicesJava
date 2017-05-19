b90df233d647fc17f92ed2b9309ca0f4b25db3fc
Tatiana <tatyana.getmanova@gmail.com>
2017-05-19 15:30:42 +0300

    Add Get by Id route in controller, update manager

--- src/main/java/com/github/spb/tget/demo/controller/SyncProfileRequestController.java
+++ src/main/java/com/github/spb/tget/demo/controller/SyncProfileRequestController.java
@@ -4,11 +4,21 @@ import static com.github.spb.tget.demo.converter.JsonDataConverter.*;
 import static spark.Spark.*;
 
 import com.github.spb.tget.demo.manager.SyncProfileRequestManager;
+import com.github.spb.tget.demo.model.SyncProfileRequest;
+
+import java.util.UUID;
 
 public class SyncProfileRequestController {
 
     public SyncProfileRequestController(final SyncProfileRequestManager syncProfileRequestManager) {
+
         get("/users", (request, response) -> syncProfileRequestManager.getSyncProfileRequests(), getJsonResponseTransformer());
+
+        get("/users/:id", (req, res) -> {
+            String idString = req.params(":id");
+            SyncProfileRequest syncProfileRequest = syncProfileRequestManager.getSyncProfileRequestById(UUID.fromString(idString));
+            return syncProfileRequest;
+        }, getJsonResponseTransformer());
     }
 }
 
--- src/main/java/com/github/spb/tget/demo/manager/SyncProfileRequestManager.java
+++ src/main/java/com/github/spb/tget/demo/manager/SyncProfileRequestManager.java
@@ -1,11 +1,13 @@
 package com.github.spb.tget.demo.manager;
 
 import com.github.spb.tget.demo.converter.UserSyncRequestConverter;
+import com.github.spb.tget.demo.data.UserEntity;
 import com.github.spb.tget.demo.model.SyncProfileRequest;
 import com.github.spb.tget.demo.repository.InProcessUserRepository;
 import com.github.spb.tget.demo.repository.UserRepository;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class SyncProfileRequestManager {
@@ -22,5 +24,22 @@ public class SyncProfileRequestManager {
                 .stream()
                 .map(item -> UserSyncRequestConverter.toSyncProfileRequest(item))
                 .collect(Collectors.toList());
+    }
+
+    public SyncProfileRequest getSyncProfileRequestById(UUID id) throws IllegalStateException
+    {
+        List<UserEntity> users = this.userRepository.getUsers();
+        UserEntity targetUser = users
+                .stream()
+                .filter(item -> item.getUserId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        if (targetUser == null)
+        {
+            throw new IllegalStateException(String.format("Not found %s", id));
+        }
+
+        return UserSyncRequestConverter.toSyncProfileRequest(targetUser);
     }
 }
