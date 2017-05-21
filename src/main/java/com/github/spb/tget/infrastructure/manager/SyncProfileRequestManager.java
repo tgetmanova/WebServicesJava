5b18e79dd495f6a2ebb0fe31f1ac92bf310c3bb5
Tatiana <tatyana.getmanova@gmail.com>
2017-05-21 14:58:38 +0300

    Add update,create to controller; extend manager with validation methods

--- src/main/java/com/github/spb/tget/infrastructure/manager/SyncProfileRequestManager.java
+++ src/main/java/com/github/spb/tget/infrastructure/manager/SyncProfileRequestManager.java
@@ -1,6 +1,5 @@
 package com.github.spb.tget.infrastructure.manager;
 
-import com.github.spb.tget.SparkBasedApp;
 import com.github.spb.tget.infrastructure.converter.UserSyncRequestConverter;
 import com.github.spb.tget.infrastructure.data.UserEntity;
 import com.github.spb.tget.infrastructure.model.SyncProfileRequest;
@@ -11,9 +10,7 @@ import java.io.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
-import java.util.Properties;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SyncProfileRequestManager {
@@ -68,13 +65,51 @@ public class SyncProfileRequestManager {
         return syncProfileRequestToCreate;
     }
 
-    public void UpdateSyncProfileRequest(SyncProfileRequest syncProfileRequest)
-    {
+    public void updateSyncProfileRequest(SyncProfileRequest syncProfileRequest) {
         SyncProfileRequest syncProfileRequestToUpdate = new SyncProfileRequest(syncProfileRequest.getUserId(),
                 UUID.randomUUID(), syncProfileRequest.getAdvertisingOptIn(),
                 LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()),
                 syncProfileRequest.getCountryIsoCode(), syncProfileRequest.getLocale());
 
         this.userRepository.updateUser(UserSyncRequestConverter.toUserEntity(syncProfileRequestToUpdate));
+    }
+
+    public void DeleteSyncProfileRequest(UUID userId) {
+        this.userRepository.deleteUser(userId);
+    }
+
+    public boolean doesUserExistInRepository(UUID userId) {
+        List<UserEntity> users = this.userRepository.getUsers();
+        return users.stream().anyMatch(item -> item.getUserId().equals(userId));
+    }
+
+    public void validateSyncProfileRequest(SyncProfileRequest request) {
+        StringJoiner validations = new StringJoiner(";");
+
+        if (!this.isCountryIsoCodeValid(request.getCountryIsoCode())) {
+            validations.add(String.format("%s is incorrect ISO code", request.getCountryIsoCode()));
+        }
+
+        if (!this.isLocaleStringValid(request.getLocale())) {
+            validations.add(String.format("%s is incorrect locale format", request.getLocale()));
+        }
+
+        String validationsString = validations.toString();
+        if (!validationsString.isEmpty()) {
+            throw new IllegalArgumentException(String.format("The Sync Profile request is invalid: %s", validationsString));
+        }
+    }
+
+    private boolean isCountryIsoCodeValid(String countryIsoCode) {
+        return countryIsoCode != null
+                && countryIsoCode.matches("[A-Z]{2}");
+    }
+
+    private boolean isLocaleStringValid(String locale) {
+        if (locale.isEmpty()) {
+            return false;
+        }
+        Locale[] locales = Locale.getAvailableLocales();
+        return Arrays.stream(locales).anyMatch(i -> i.toString().equalsIgnoreCase(locale));
     }
 }
--- src/main/java/com/github/spb/tget/restapi/spring/demo/SyncProfileRequestController.java
+++ src/main/java/com/github/spb/tget/restapi/spring/demo/SyncProfileRequestController.java
@@ -33,6 +33,44 @@ public class SyncProfileRequestController {
         } catch (IllegalStateException exception) {
             return new ResponseEntity(String.format("Profile with ID: %s is not found", id), HttpStatus.NOT_FOUND);
         }
+
         return new ResponseEntity(targetProfile, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/profiles", method = RequestMethod.POST)
+    public ResponseEntity<SyncProfileRequest> createSyncProfileRequest(@RequestBody SyncProfileRequest syncProfileRequest) {
+        try {
+            this.syncProfileRequestManager.validateSyncProfileRequest(syncProfileRequest);
+        } catch (IllegalArgumentException exception) {
+            return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        if (this.syncProfileRequestManager.doesUserExistInRepository(syncProfileRequest.getUserId())) {
+            this.syncProfileRequestManager.updateSyncProfileRequest(syncProfileRequest);
+            return new ResponseEntity(String.format("Profile with ID: %s has been updated", syncProfileRequest.getUserId()),
+                    HttpStatus.NO_CONTENT);
+        }
+
+        SyncProfileRequest newlyCreatedProfile = this.syncProfileRequestManager.createSyncProfileRequest(syncProfileRequest);
+        return new ResponseEntity(newlyCreatedProfile, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/profiles/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateSyncProfileRequest(@PathVariable("id") UUID id, @RequestBody SyncProfileRequest syncProfileRequest) {
+        try {
+            this.syncProfileRequestManager.validateSyncProfileRequest(syncProfileRequest);
+        } catch (IllegalArgumentException exception) {
+            return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            this.syncProfileRequestManager.getSyncProfileRequestById(syncProfileRequest.getUserId());
+        } catch (IllegalStateException exception) {
+            return new ResponseEntity(String.format("Failed to update profile: profile with ID: %s is not found", syncProfileRequest.getUserId()),
+                    HttpStatus.NOT_FOUND);
+        }
+
+        this.syncProfileRequestManager.updateSyncProfileRequest(syncProfileRequest);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
